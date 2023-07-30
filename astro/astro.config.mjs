9931baff0112416de77613ec90c773432e600f78
tubstrr <me@jonknoll.dev>
2023-07-30 11:42:42 -0500

    update: Serverless for stale-while-revalidate

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
